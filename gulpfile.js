c10d448d53e801c4bd65be56e7219b8af6dd76e7
Mia <joeschen0421@gmail.com>
2018-12-14 11:42:01 +0800

    新增 gulp-connect

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
+const connect = require('gulp-connect');
 
 var path = {
     tsPath: ["src/**/*.ts"],
 };
+
+gulp.task('default',['watch','connect']);
+gulp.task('connect', function() {
+    connect.server();
+});
 
 gulp.task('tsc', function () {
     return gulp.src('src/**/*.ts')
